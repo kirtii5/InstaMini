a0dfbb6b1ad1890ce370e4d5dd9f63e06e630457
Kirti Choudhary <kirtimmd2@gmail.com>
2025-12-14 19:33:30 +0530

    added / route

--- server/server.js
+++ server/server.js
@@ -16,10 +16,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.status(200).send("InstaMini API is running 🚀");
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api", interactionRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
